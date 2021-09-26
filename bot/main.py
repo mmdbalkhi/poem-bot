6ef50e43d820ce4d05816b342b1d21506d09aefa
mmdbalkhi <ahmdparsh129@gmail.com>
2021-09-26 18:25:16 +0330

    first bot commit

---
+++ bot/main.py
@@ -0,0 +1,58 @@
+#! venv/bin/python3
+"""a little bot for send paom at One houre to Discord Chanell
+"""
+import logging
+from datetime import datetime
+from json import loads
+from typing import AnyStr, Dict
+
+import discord
+from discord.ext import commands
+from requests import get
+
+from config import TOKEN
+
+bot = commands.Bot(command_prefix="/")
+
+logger = logging.getLogger("discord")
+
+logging.basicConfig(level=logging.INFO)
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(handler)
+
+
+def get_random_poem_from_ganjoor() -> Dict:
+
+    content = get("http://c.ganjoor.net/beyt-json.php").content
+    json_poem = loads(content)
+
+    m1 = json_poem["m1"]
+    m2 = json_poem["m2"]
+    return {"m1": m1, "m2": m2, "شاعر": json_poem["poet"]}
+
+
+def send_poem_for_user() -> AnyStr:
+    poem_poet = get_random_poem_from_ganjoor()
+
+    return f"{poem_poet['m1']} // {poem_poet['m2']}\n" f"{poem_poet['شاعر']}\n"
+
+
+@bot.command(name="poem")
+async def poem(ctx):
+    await ctx.send(send_poem_for_user())
+
+
+@bot.command(name="شعر")
+async def poem(ctx):
+    await ctx.send(send_poem_for_user())
+
+
+@bot.command(name="بیت")
+async def poem(ctx):
+    await ctx.send(send_poem_for_user())
+
+
+bot.run(TOKEN)
